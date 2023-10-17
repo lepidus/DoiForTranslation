45216c663e487ab08f7b667d53a9c611681d815a
Jhon <jhon@lepidus.com.br>
2023-10-17 18:12:47 -0400

    Adds integration test for hidding of locales in form
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#657
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test3_workflowFeatures.spec.js
+++ cypress/tests/Test3_workflowFeatures.spec.js
@@ -5,11 +5,24 @@ describe('Submissions Translation - Workflow features', function () {
         cy.get('.pkpButton:visible:contains("View")').eq(1).click();
 
         cy.contains('Translations').click();
-        cy.get('.pkpPublication__translations a:contains("Français (Canada)")');
         cy.get('.pkpPublication__translations li a').should('have.length', 1);
         cy.get('.pkpPublication__translations a:contains("Français (Canada)")').click();
 
         cy.contains('Plugin de test pour créer une traduction de soumission');
+    });
+    it('Locales are hidden from create translation form', function() {
+        cy.login('dbarnes', null, 'publicknowledge');
+        cy.get('#active-button').click();
+        cy.get('.pkpButton:visible:contains("View")').eq(1).click();
+
+        cy.get('button:contains("Create translation")').click();
+        cy.get('select[name="translationLocale"] option[value="en_US"]').should('not.exist');
+        cy.get('select[name="translationLocale"] option[value="fr_CA"]').should('not.exist');
+        cy.get('select[name="translationLocale"]').select('pt_BR');
+        cy.get('#createTranslationModal button:contains("Create")').click();
+
+        cy.get('button:contains("Create translation")').click();
+        cy.contains('Translations have already been created for this submission in all languages available in this journal');
     });
     it('Reference to translated submission on translation submission workflow', function () {
         cy.login('dbarnes', null, 'publicknowledge');
